eeb52df26fe6bb892cabef69594eeb8d21a1e808
Emmy White <emmalinerosewhite@gmail.com>
2020-01-20 17:51:05 +0000

    Automatically backed up by Learn

--- db/migrate/20200120175015_create_preferences.rb
+++ db/migrate/20200120175015_create_preferences.rb
@@ -1,6 +1,10 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
   def change
     create_table :preferences do |t|
+      t.boolean :allow_create_artists
+      t.boolean :allow_create_songs
+      
+      
     end
   end
 end
