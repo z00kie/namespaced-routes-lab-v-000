9ed50a8ef8dd1d55a4a7b653bf6866eb15470cf2
Emmy White <emmalinerosewhite@gmail.com>
2020-01-20 17:36:45 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,7 +12,8 @@ class ArtistsController < ApplicationController
     if @preference.allow_create_artists
       @artist = Artist.new
     else
-      redirect_to artists_
+      redirect_to artists_path
+    end
   end
 
   def create
